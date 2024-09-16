14e9e7650758c17051ae9dbfad8d7567eca27046
mini tesuda <yasmin.nascimento50@aluno.ce.gov.br>
2024-09-16 16:36:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let vehicle1 = prompt('Digite o nome do primeiro veículo:')
-let vehicle2 = prompt('Digite o nome do segundo veículo:')
-const vehicleSpeed1 = prompt(`Digite a velocidade do primeiro veículo:`)
-const vehicleSpeed2 = prompt(`Digite a velocidade do segundo veículo:`)
+let vehicle1 = prompt('Digite o nome do primeiro veículo ')
+let speed1 = prompt('Digite a velocidade do primeiro veículo ')
+let vehicle2 = prompt('Digite o nome do segundo veículo ')
+let speed2 = prompt('Digite a velocidade do segundo veículo ')
+if (speed1>speed2) {
+    alert(`O ${vehicle1} está a ${speed1}km/h, e está mais rápido que o ${vehicle2} que está a ${speed2}km/h`)
+   
+} else if (speed1<speed2)
+        alert(`O ${vehicle2} está a ${speed2}km/h, e está mais rápido que o ${vehicle1} que está a ${speed1}km/h.`);
+    
+else {(speed1===speed2)
+alert(`Tanto ${vehicle1} e ${vehicle2} estão na mesma velocidade de ${speed1}km/h`)
 
-if (vehicleSpeed1 > vehicleSpeed2) {
-    alert(`${vehicle1} está a ${vehicleSpeed1}km/h, e mais rápido que o ${vehicle2}, que está a ${vehicleSpeed2}km/h.`)
-} else if (vehicleSpeed1 < vehicleSpeed2) {
-    alert(`${vehicle2} está a ${vehicleSpeed2}km/h, e mais rápido que o ${vehicle1}, que está a ${vehicleSpeed1}km/h.`)
-} else {
-    alert(`Tanto ${vehicle1} e ${vehicle2} estão a ${vehicleSpeed1 || vehicleSpeed2}km/h.`)
-}
+     }let vehicle1 = prompt('Digite o nome do primeiro veículo ')
+let speed1 = prompt('Digite a velocidade do primeiro veículo ')
+let vehicle2 = prompt('Digite o nome do segundo veículo ')
+let speed2 = prompt('Digite a velocidade do segundo veículo ')
+if (speed1>speed2) {
+    alert(`O ${vehicle1} está a ${speed1}km/h, e está mais rápido que o ${vehicle2} que está a ${speed2}km/h`)
+   
+} else if (speed1<speed2)
+        alert(`O ${vehicle2} está a ${speed2}km/h, e está mais rápido que o ${vehicle1} que está a ${speed1}km/h.`);
+    
+else {(speed1===speed2)
+alert(`Tanto ${vehicle1} e ${vehicle2} estão na mesma velocidade de ${speed1}km/h`)
+
+     }
